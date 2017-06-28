33ccc0a71c2268591f8096ded03c52712ea62003
RangiBecerra <rangibecerra@gmail.com>
2017-06-27 23:27:29 -0400

    ejercicio guiado terminado

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //alert("existe");
 $ (document).ready (function() {
  	// codigo
+ 	$("input:button").click(function(){
+ 	btn = $(this).val();
+ 	pantalla = $("#resultado").val();
+ 	if(btn=="C"){
+ 		$("#resultado").val("");
+ 	} else {
+ 		if(btn=="=") {
+ 			$("#resultado").val(eval(pantalla));
+ 		} else {
+ 			$("#resultado").val(pantalla + btn);
+ 		}
+ 	}
+ 	})
 });
